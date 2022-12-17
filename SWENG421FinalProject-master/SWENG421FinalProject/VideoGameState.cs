53b352c8257e00851c2e6b0abf19a9672c1004d0
SnackerSnake <67903971+SnackerSnake@users.noreply.github.com>
2022-12-16 23:15:17 -0500

    Add files via upload

---
+++ SWENG421FinalProject-master/SWENG421FinalProject/VideoGameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWENG421FinalProject.Models.State
+{
+    public enum VideoGameStateEnum
+    {
+        Choice = 1,
+        Fight = 2,
+        Inventory = 3
+    }
+
+    public abstract class VideoGameState
+    {
+        public int stateNum { get; set; }
+
+        public abstract void setState(Context s);
+
+    }
+}
