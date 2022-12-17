0fb4756fbf0b2290852cfd098c01d65c74fb93c0
SnackerSnake <67903971+SnackerSnake@users.noreply.github.com>
2022-12-16 23:18:39 -0500

    Add files via upload

---
+++ SWENG421FinalProject-master/SWENG421FinalProject/Models/AbsInputFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWENG421FinalProject.Models.Filter
+{
+    public abstract class AbsInputFilter : InputFilterIF
+    {
+        public abstract string GetInput(string input);
+        public abstract bool Validate(string input);
+    }
+}
