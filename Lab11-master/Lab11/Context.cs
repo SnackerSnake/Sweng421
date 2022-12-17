b466ed5f55e045c52f2e5e34ba91b118882d390f
SnackerSnake <67903971+SnackerSnake@users.noreply.github.com>
2022-12-16 23:08:53 -0500

    Add files via upload

---
+++ Lab11-master/Lab11/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11.Models
+{
+    public class Context
+    {
+        private CalculatorState state { get; set; }
+
+        public Context()
+        {
+            state = null;
+        }
+
+        public void setState(CalculatorState s)
+        {
+            this.state = s;
+            this.state.stateNum = s.stateNum;
+        }
+
+        public CalculatorState getState()
+        {
+            return this.state;
+        }
+    }
+}
