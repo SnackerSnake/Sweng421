120d7788afa16f6b77c6370aa3f50ee825ffe4f4
SnackerSnake <67903971+SnackerSnake@users.noreply.github.com>
2022-12-16 23:19:22 -0500

    Add files via upload

---
+++ SWENG421FinalProject-master/SWENG421FinalProject/Models/AbstractLoadableItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWENG421FinalProject.Models.DynamicLinkage
+{
+    public abstract class AbstractLoadableItem
+    {
+        public abstract AverageItem Load(string fileName);
+    }
+}
